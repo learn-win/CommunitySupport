1b4b3bd10a5760fb1e4706868dd77378d6dcff81
ard <ard@test.com>
2025-05-04 15:31:46 +0530

    build check

--- main.jsx
+++ main.jsx
@@ -2,8 +2,8 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import App from "./App.jsx";
-import { AuthProvider } from "./context/AuthContext";
+import App from "./src/App.jsx";
+import { AuthProvider } from "./src/context/AuthContext.jsx";
 
 createRoot(document.getElementById("root")).render(
   <AuthProvider>
--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
 
-  base: '/CommunitySupport/', // 👈 IMPORTANT
+  // base: '/CommunitySupport/', // 👈 IMPORTANT
 
 });
